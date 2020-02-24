800a6c515bdd3e20dd9164cb4edd742c235b07ec
aden <eleyaden@gmail.com>
2020-02-24 15:34:01 +0000

    -final changes before meeting

--- app/src/main/java/com/example/barberme001/SignUp.java
+++ app/src/main/java/com/example/barberme001/SignUp.java
@@ -7,12 +7,16 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
+import android.provider.ContactsContract;
 import android.view.View;
 
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+
+import com.example.model.User;
+import com.example.sql.DatabaseHelper;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -24,6 +28,8 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
 
     EditText etName, etEmail, etPassword;
     Button button_register;
+    DatabaseHelper databaseHelper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +41,8 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
         etPassword = (EditText) findViewById(R.id.editText_password);
         button_register = (Button) findViewById(R.id.button_register);
 
+        databaseHelper = new DatabaseHelper(this);
+
 
         //create the listener for the register button
         button_register.setOnClickListener(this);
@@ -42,7 +50,26 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.button_register) {
-            //runs when register button is clicked
+            String email = etEmail.getText().toString().trim();
+            if (!databaseHelper.checkUser(email)) {
+                String userName = etName.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+
+                User user = new User();
+                user.setName(userName);
+                user.setPassword(password);
+                user.setEmail(email);
+
+                databaseHelper.addUser(user);
+
+                //return
+                Toast toast = Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_SHORT);
+                toast.show();
+                finish();
+            } else {
+                Toast toast = Toast.makeText(getApplicationContext(), "Account already exists with that email", Toast.LENGTH_SHORT);
+                toast.show();
+            }
 
 
         }
