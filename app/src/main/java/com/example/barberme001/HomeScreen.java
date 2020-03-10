577fb6b0f047bfa1c4052baba483d6fc53dae354
adeneels <eleyaden@gmail.com>
2020-03-10 12:04:59 +0000

    Basic functionality to nav bar #2 Added fragments Each button now switches fragment and displays it successfully Up next: implement content of each fragment

--- app/src/main/java/com/example/barberme001/HomeScreen.java
+++ app/src/main/java/com/example/barberme001/HomeScreen.java
@@ -1,5 +1,6 @@
 package com.example.barberme001;
 
+import androidx.fragment.app.Fragment;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -7,6 +8,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -35,22 +37,38 @@ public class HomeScreen extends AppCompatActivity {
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem item) {
+                Fragment fragment;
                 switch (item.getItemId()) {
                     case R.id.navigation_home:
+                        fragment = new HomeFragment();
+                        loadFragment(fragment);
                         title.setText("HOME");
                         return true;
                     case R.id.navigation_explore:
+                        fragment = new ExploreFragment();
+                        loadFragment(fragment);
                         title.setText("EXPLORE");
                         return true;
                     case R.id.navigation_notifications:
-                        title.setText("NOTIFICATIONS");
+                        fragment = new InboxFragment();
+                        loadFragment(fragment);
+                        title.setText("INBOX");
                         return true;
                     case R.id.navigation_profile:
+                        fragment = new ProfileFragment();
+                        loadFragment(fragment);
                         title.setText("PROFILE");
                         return true;
                 }
                 return false;
             }
         });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.topcontainer, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
